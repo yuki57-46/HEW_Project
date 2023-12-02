627bd6302bad4d57b46cdf74823a19243236e8b2
yuki57-46 <yuki.rno@gmail.com>
2023-12-02 11:07:43 +0900

    リフトのcppの実装（未完）

--- HEW_Project/Lift_Obj.cpp
+++ HEW_Project/Lift_Obj.cpp
@@ -1,4 +1,59 @@
 #include "Lift_Obj.hpp"
 
+Lift_Obj::Lift_Obj()
+{
+}
 
+Lift_Obj::~Lift_Obj()
+{
+}
 
+void Lift_Obj::Update()
+{
+}
+
+void Lift_Obj::Draw(DirectX::XMFLOAT4X4 viewMatrix, DirectX::XMFLOAT4X4 projectionMatrix)
+{
+}
+
+void Lift_Obj::SetBounds(const DirectX::XMFLOAT3& min, const DirectX::XMFLOAT3& max)
+{
+}
+
+DirectX::XMFLOAT3 Lift_Obj::GetminBounds()
+{
+	return DirectX::XMFLOAT3();
+}
+
+DirectX::XMFLOAT3 Lift_Obj::GetmaxBounds()
+{
+	return DirectX::XMFLOAT3();
+}
+
+DirectX::XMFLOAT3 Lift_Obj::Add(const DirectX::XMFLOAT3& a, const DirectX::XMFLOAT3& b)
+{
+	return DirectX::XMFLOAT3();
+}
+
+void Lift_Obj::CSetBounds(const DirectX::XMFLOAT3& min, const DirectX::XMFLOAT3& max)
+{
+}
+
+DirectX::XMFLOAT3 Lift_Obj::CGetminBounds()
+{
+	return DirectX::XMFLOAT3();
+}
+
+DirectX::XMFLOAT3 Lift_Obj::CGetmaxBounds()
+{
+	return DirectX::XMFLOAT3();
+}
+
+DirectX::XMFLOAT3 Lift_Obj::CAdd(const DirectX::XMFLOAT3& a, const DirectX::XMFLOAT3& b)
+{
+	return DirectX::XMFLOAT3();
+}
+
+void Lift_Obj::LiftObj_SetPos()
+{
+}
