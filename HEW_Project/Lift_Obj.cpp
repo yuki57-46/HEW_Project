af0f42486f3be8c6e79c3641073af888340a647b
yuki57-46 <yuki.rno@gmail.com>
2023-12-02 13:37:34 +0900

    Lift_Obj クラスのコンストラクタとデストラクタの実装

--- HEW_Project/Lift_Obj.cpp
+++ HEW_Project/Lift_Obj.cpp
@@ -1,11 +1,54 @@
 #include "Lift_Obj.hpp"
+#include "Geometory.h"
+
+
+// プレイヤーとの当たり判定用
+DirectX::XMFLOAT3 liftobj_MinBound = DirectX::XMFLOAT3(-0.5f, -0.5f, -0.5f);
+DirectX::XMFLOAT3 liftobj_MaxBound = DirectX::XMFLOAT3(0.5f, 0.5f, 0.5f);
+
+// ブロック同士の当たり判定用
+DirectX::XMFLOAT3 c_liftobj_MinBound = DirectX::XMFLOAT3(-0.5f, -0.5f, -0.5f);
+DirectX::XMFLOAT3 c_liftobj_MaxBound = DirectX::XMFLOAT3(0.5f, 0.5f, 0.5f);
 
 Lift_Obj::Lift_Obj()
+	: m_pos(0.0f, 0.0f, 0.0f), m_oldPos(0.0f, 0.0f, 0.0f)
+	, m_scale(1.0f, 1.0f, 1.0f)
+	, m_rotY(0.0f), m_lastRotY(0.0f)
+	, m_direction(0.0f, 0.0f, 1.0f)
+	, m_rotMat(DirectX::XMMatrixIdentity())
+	, moveOk(false)
+	, m_pModel(nullptr), m_pVS(nullptr)
 {
+	m_pModel = new Model();
+	if (!m_pModel->Load("Assets/Model/Block/test_black_cube.fbx", 0.05f, Model::Flip::XFlip))
+	{
+		MessageBox(nullptr, "Liftモデルの読み込みに失敗しました", "エラー", MB_OK | MB_ICONWARNING);
+	}
+	m_pVS = new VertexShader();
+	if (!m_pVS->Load("Assets/Shader/VS_Model.cso"))
+	{
+		MessageBox(nullptr, "Lift頂点シェーダーの読み込みに失敗しました", "エラー", MB_OK | MB_ICONWARNING);
+	}
+	m_pModel->SetVertexShader(m_pVS);
+
+	// それぞれの当たり判定の大きさを設定
+	SetBounds(liftobj_MinBound, liftobj_MaxBound);
+	CSetBounds(c_liftobj_MinBound, c_liftobj_MaxBound);
+
 }
 
 Lift_Obj::~Lift_Obj()
 {
+	if (m_pVS)
+	{
+		delete m_pVS;
+		m_pVS = nullptr;
+	}
+	if (m_pModel)
+	{
+		delete m_pModel;
+		m_pModel = nullptr;
+	}
 }
 
 void Lift_Obj::Update()
