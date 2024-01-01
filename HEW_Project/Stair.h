f14bcc7d8e05fe661b78eeec622c593bfffe809e
yuki57-46 <yuki.rno@gmail.com>
2024-01-01 23:56:28 +0900

    tmp

---
+++ HEW_Project/Stair.h
@@ -0,0 +1,102 @@
+#pragma once
+#include "CameraBase.h"
+#include "GameObject.h"
+#include "Model.h"
+#include "Shader.h"
+#include"Soundtest.h"
+
+class Stair : public GameObject
+{
+public:
+	Stair();
+	~Stair();
+
+	void Update();
+	void Draw(DirectX::XMFLOAT4X4 viewMatrix, DirectX::XMFLOAT4X4 projectionMatrix);
+	void SetBounds(const DirectX::XMFLOAT3& min, const DirectX::XMFLOAT3& max);
+	DirectX::XMFLOAT3 GetminBounds();
+	DirectX::XMFLOAT3 GetmaxBounds();
+	DirectX::XMFLOAT3 Add(const DirectX::XMFLOAT3& a, const DirectX::XMFLOAT3& b);
+
+	void HSetBounds(const DirectX::XMFLOAT3& min, const DirectX::XMFLOAT3& max);
+	DirectX::XMFLOAT3 HGetminBounds();
+	DirectX::XMFLOAT3 HGetmaxBounds();
+	DirectX::XMFLOAT3 HAdd(const DirectX::XMFLOAT3& a, const DirectX::XMFLOAT3& b);
+
+
+	void CSetBounds(const DirectX::XMFLOAT3 & min, const DirectX::XMFLOAT3 & max);
+	DirectX::XMFLOAT3 CGetminBounds();
+	DirectX::XMFLOAT3 CGetmaxBounds();
+	DirectX::XMFLOAT3 CAdd(const DirectX::XMFLOAT3& a, const DirectX::XMFLOAT3& b);
+
+	DirectX::XMFLOAT3 GetStairMaxBounds();
+	DirectX::XMFLOAT3 GetPos();
+
+	void Create(float posX, float posY, float posZ, float scaleX, float scaleY, float scaleZ);
+	void Set();
+	void SetF();
+	bool SetR();
+
+
+	void OBJPos();
+	void OBJPosy();
+	void Modelchg();
+	void Modelchg2();
+
+
+	void Set1();
+	void SetF1();
+	void SetColgravity();
+	void SetStairTop();
+
+	void MoveStair(float y);
+
+	bool SetR1();
+	bool IsGravity();
+	bool IsXZ();
+	bool IsStairTop();
+
+	void ExtractSlopeVertexCoordinates(Model& slopeModel);
+
+
+private:
+	Model* m_pStairModel;
+	VertexShader* m_pStairVS;
+	DirectX::XMFLOAT3 m_oldPos;
+	DirectX::XMFLOAT3 m_pos;
+	DirectX::XMFLOAT3 m_scale;
+	DirectX::XMFLOAT3 m_jmp;
+	DirectX::XMFLOAT3 m_mmovespeed;
+
+	float m_rotationY;
+	DirectX::XMFLOAT3 m_direction;
+	DirectX::XMMATRIX m_rotationMatrix;
+	float m_lastRotationY;
+	bool moveok;
+
+
+	bool ok;
+	bool gravity;
+	bool colgravity;
+	bool xz;
+	bool StairTop;
+	float a = 0.0f;
+
+
+
+	IXAudio2SourceVoice* m_pSVSEBlk; // サウンドソース
+	XAUDIO2_BUFFER* m_pSDSEBlk; // サウンドバッファ
+
+	DirectX::XMFLOAT3 StairMinBound;
+	DirectX::XMFLOAT3 StairMaxBound;
+
+	DirectX::XMFLOAT3 hStairMinBound;
+	DirectX::XMFLOAT3 hStairMaxBound;
+
+	DirectX::XMFLOAT3 cStairMinBound;
+	DirectX::XMFLOAT3 cStairMaxBound;
+
+	int frame = 50;
+
+};
+
