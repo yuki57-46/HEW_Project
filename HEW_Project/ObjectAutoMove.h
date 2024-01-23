f8a8217aeaa738140cf7313eada8d771ade36d9d
yuki57-46 <yuki.rno@gmail.com>
2024-01-23 10:31:47 +0900

    復元

--- HEW_Project/ObjectAutoMove.h
+++ HEW_Project/ObjectAutoMove.h
@@ -12,19 +12,19 @@ public:
 	void Update();
 	void Draw(DirectX::XMFLOAT4X4 viewMatrix, DirectX::XMFLOAT4X4 projectionMatrix);
 
-	void SetBounds(const DirectX::XMFLOAT3& min, const DirectX::XMFLOAT3& max);
+	/*void SetBounds(const DirectX::XMFLOAT3& min, const DirectX::XMFLOAT3& max);
 	DirectX::XMFLOAT3 GetminBounds();
-	DirectX::XMFLOAT3 GetmaxBounds();
+	DirectX::XMFLOAT3 GetmaxBounds();*/
 	DirectX::XMFLOAT3 Add(const DirectX::XMFLOAT3& a, const DirectX::XMFLOAT3& b);
 
-	void HSetBounds(const DirectX::XMFLOAT3& min, const DirectX::XMFLOAT3& max);
-	DirectX::XMFLOAT3 HGetminBounds();
-	DirectX::XMFLOAT3 HGetmaxBounds();
-	DirectX::XMFLOAT3 HAdd(const DirectX::XMFLOAT3& a, const DirectX::XMFLOAT3& b);
+	//void HSetBounds(const DirectX::XMFLOAT3& min, const DirectX::XMFLOAT3& max);
+	//DirectX::XMFLOAT3 HGetminBounds();
+	//DirectX::XMFLOAT3 HGetmaxBounds();
+	//DirectX::XMFLOAT3 HAdd(const DirectX::XMFLOAT3& a, const DirectX::XMFLOAT3& b);
 
-	void CSetBounds(const DirectX::XMFLOAT3& min, const DirectX::XMFLOAT3& max);
+	/*void CSetBounds(const DirectX::XMFLOAT3& min, const DirectX::XMFLOAT3& max);
 	DirectX::XMFLOAT3 CGetminBounds();
-	DirectX::XMFLOAT3 CGetmaxBounds();
+	DirectX::XMFLOAT3 CGetmaxBounds();*/
 	DirectX::XMFLOAT3 CAdd(const DirectX::XMFLOAT3& a, const DirectX::XMFLOAT3& b);
 
 	void CreateAuto(float posX, float posY, float posZ, float scaleX, float scaleY, float scaleZ);
@@ -51,6 +51,11 @@ private:
 	DirectX::XMMATRIX m_rotationMatrix;
 	float m_lastRotationY;
 	bool moveok;
+
+	float m_MaxPosX;	// X軸の限界範囲(右)
+	float m_MinPosX;	// X軸の限界範囲(左)
+
+	bool m_MoveX;	// オブジェクトの自動判定
 
 	bool m_isPossessed;
 };
